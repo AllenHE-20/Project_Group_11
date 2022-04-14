10cee5157d83b3d1c29240d655dd8d6e2df5aeaa
AllenHE-20 <allenalvis@gmail.com>
2022-04-14 08:08:33 -0500

    Added controller to createQuestion and function triggered on submit button press to CreateQuestionController

--- src/main/java/testformgenerator/project_group_11/CreateQuestionController.java
+++ src/main/java/testformgenerator/project_group_11/CreateQuestionController.java
@@ -1,4 +1,45 @@
 package testformgenerator.project_group_11;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class CreateQuestionController {
+    public CreateQuestionController(){
+        //Empty constructor
+    }
+
+    @FXML
+    private Button homeButton;
+
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    private TextField questField;
+
+    @FXML
+    private TextField ansAField;
+
+    @FXML
+    private TextField ansBField;
+
+    @FXML
+    private TextField ansCField;
+
+    @FXML
+    private TextField ansDField;
+
+
+
+
+    public void tryCreateQuestion(ActionEvent actionEvent) {
+        submitQuestion();
+    }
+
+    private void submitQuestion(){ //Check if this needs to throw an exception
+        //
+
+    }
 }
