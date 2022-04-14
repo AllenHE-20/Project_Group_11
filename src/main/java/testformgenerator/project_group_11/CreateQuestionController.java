b39abe874be6f105eca325d98f6eb728730943fc
AllenHE-20 <allenalvis@gmail.com>
2022-04-14 09:30:48 -0500

    Question is now evaluated for correctness

--- src/main/java/testformgenerator/project_group_11/CreateQuestionController.java
+++ src/main/java/testformgenerator/project_group_11/CreateQuestionController.java
@@ -3,18 +3,23 @@ package testformgenerator.project_group_11;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class CreateQuestionController {
-    public CreateQuestionController(){
-        //Empty constructor
-    }
+
 
     @FXML
     private Button homeButton;
 
     @FXML
     private Button submitButton;
+
+    /*
+    @FXML
+    private ChoiceBox<Integer> correctAnswerBox;
+    */
 
     @FXML
     private TextField questField;
@@ -31,6 +36,20 @@ public class CreateQuestionController {
     @FXML
     private TextField ansDField;
 
+    @FXML
+    private Label invalidQuestionLabel;
+
+    public CreateQuestionController(){ //Choicebox does not work yet
+        //Empty constructor
+        /*
+        correctAnswerBox = new ChoiceBox<Integer>();
+        correctAnswerBox.getItems().add(1);
+        correctAnswerBox.getItems().add(2);
+        correctAnswerBox.getItems().add(3);
+        correctAnswerBox.getItems().add(4);
+        */
+
+    }
 
 
 
@@ -39,7 +58,18 @@ public class CreateQuestionController {
     }
 
     private void submitQuestion(){ //Check if this needs to throw an exception
-        //
+        String[] answers = {ansAField.getText(), ansBField.getText(), ansCField.getText(), ansDField.getText()};
+
+
+        String result = createQuestion.createQuestionAction(questField.getText(), answers, 1);
+
+        if (result.equals("Created Question Sucessfully")){
+            //Add to question bank, change scene
+        }else{
+            //Set invalidQuestionLabel message to result
+            invalidQuestionLabel.setText(result);
+        }
+
 
     }
 }
