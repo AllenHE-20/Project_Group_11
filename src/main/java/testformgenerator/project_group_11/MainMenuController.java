41f81897d23a406e3542704685a4ff3e064e996e
AllenHE-20 <allenalvis@gmail.com>
2022-04-26 15:15:04 -0500

    Added all buttons to main menu, set transitions to implemented creators

--- src/main/java/testformgenerator/project_group_11/MainMenuController.java
+++ src/main/java/testformgenerator/project_group_11/MainMenuController.java
@@ -15,6 +15,37 @@ public class MainMenuController {
     @FXML
     private Button createBank;
 
+    @FXML
+    private Button createQuestion;
+
+    @FXML
+    private Button createTest;
+
+    public void changeScene(ActionEvent actionEvent){
+        //Code to determine button pressed goes here
+
+        if(actionEvent.getSource() == createBank){
+            changeSceneHandler("QuestionBank.fxml");
+        }else if(actionEvent.getSource() == createQuestion){
+            changeSceneHandler("createQuestion.fxml");
+        }else if(actionEvent.getSource() == createTest){
+            changeSceneHandler("createTestUI.fxml");
+        }
+    }
+
+    private void changeSceneHandler(String target){
+        Stage stage;
+        Parent root;
+        try {
+            stage = (Stage) createBank.getScene().getWindow();
+            root = FXMLLoader.load(getClass().getResource(target));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }catch(IOException e){
+            System.err.println("An error " + e.getMessage() + " occurred when switching to the create question scene.");
+        }
+    }
 
     public void changeToBank(ActionEvent actionEvent) throws IOException {createBankSceneChange();}
 
