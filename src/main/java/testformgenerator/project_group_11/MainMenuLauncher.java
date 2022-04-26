e97984a3c819dce4e1c17eb8d9b13a4b194b8bd0
AllenHE-20 <allenalvis@gmail.com>
2022-04-26 15:15:35 -0500

    Added creation of demo database to launcher

--- src/main/java/testformgenerator/project_group_11/MainMenuLauncher.java
+++ src/main/java/testformgenerator/project_group_11/MainMenuLauncher.java
@@ -11,6 +11,16 @@ public class MainMenuLauncher extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(createQuestionDemo.class.getResource("mainMenu.fxml"));
+        DBMgrHolder dbSetup = DBMgrHolder.getInstance();
+        DBMgr data = new DBMgr();
+        QuestionBank qb = new QuestionBank("Bank 1");
+        Question q = new Question("What is today's date?", "July 24th, 2022", "August 16th, 2022", "January 24th, 2022", "April 21st, 2022");
+        Question q1 = new Question("What is today's date?", "July 24th, 2022", "August 16th, 2022", "January 24th, 2022", "April 21st, 2022");
+        qb.addNewQuestion(q);
+        qb.addNewQuestion(q1);
+        data.addBank(qb);
+        dbSetup.setDatabase(data);
+
         Scene scene = new Scene(fxmlLoader.load(), 590, 400);
         stage.setTitle("Test Creator");
         stage.setScene(scene);
