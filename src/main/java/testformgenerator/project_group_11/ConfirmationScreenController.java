6d765e1658f5a1ceac1202069d68f1ec2f479cc2
AllenHE-20 <allenalvis@gmail.com>
2022-04-27 16:50:15 -0500

    Added content to confirmation screen

--- src/main/java/testformgenerator/project_group_11/ConfirmationScreenController.java
+++ src/main/java/testformgenerator/project_group_11/ConfirmationScreenController.java
@@ -1,4 +1,42 @@
 package testformgenerator.project_group_11;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ConfirmationScreenController {
+
+    @FXML
+    private Button okButton;
+
+    @FXML
+    private Label messageLabel;
+
+    @FXML
+    public void initialize(){
+        DBMgrHolder holder = DBMgrHolder.getInstance();
+        DBMgr database = holder.getDatabase();
+
+        messageLabel.setText(database.getPersistentMessage());
+    }
+
+    public void goToMenu(){
+        Stage stage;
+        Parent root;
+        try {
+            stage = (Stage) okButton.getScene().getWindow();
+            root = FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }catch(IOException e){
+            System.err.println("An error " + e.getMessage() + " occurred when switching to the create question scene.");
+        }
+    }
 }
