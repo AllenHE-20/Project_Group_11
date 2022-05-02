abe5e30b8195bb2eceb5f8e063d190ec9c180197
AllenHE-20 <allenalvis@gmail.com>
2022-05-01 20:49:13 -0500

    Added comments to tests

--- src/test/java/QuestionBankTest.java
+++ src/test/java/QuestionBankTest.java
@@ -20,23 +20,23 @@ class QuestionBankTest {
     }
 
     @Test
-    void test_one(){
+    void test_one(){ //Valid
         assertEquals("Created Question Bank Successfully", QuestionBank.createQuestionBankAction("set1", testDB));
     }
 
     @Test
-    void test_two(){
+    void test_two(){ //Invalid
         testDB.addBank(new QuestionBank("set1"));
         assertEquals("Name has been used before, please use a new name.", QuestionBank.createQuestionBankAction("set1", testDB));
     }
 
     @Test
-    void test_three(){
+    void test_three(){ //Exception
         assertEquals("Name does not fit the character limit, please try again.", QuestionBank.createQuestionBankAction("", testDB));
     }
 
     @Test
-    void test_four(){
+    void test_four(){ //Exception
         assertEquals("Name does not fit the character limit, please try again.", QuestionBank.createQuestionBankAction("CS3354 Exam 1 Set 1 2022 Spring May-June - Narayanasami", testDB));
     }
 
--- src/test/java/createQuestionTest.java
+++ src/test/java/createQuestionTest.java
@@ -13,43 +13,43 @@ class createQuestionTest {
     }
 
     @Test
-    void test_two(){
+    void test_two(){ //Invalid question
         String[] a = {"1", "3", "4", "5"};
         assertEquals("Invalid Question Input", createQuestionAction("What is 2+222+222222+22222222222222222222+222222222222222+222222222222222222222222+2222222+22222222222222222+2222222222+2222222222222+2222222222222+222222222222222222222222222222222222?", a, 3));
     }
 
     @Test
-    void test_three(){
+    void test_three(){ //Exceptional question
         String[] a = {"1", "3", "4", "5"};
         assertEquals("Invalid Question Input", createQuestionAction("", a, 3));
     }
 
     @Test
-    void test_four(){
+    void test_four(){ //Exceptional answer
         String[] a = {"1", "333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333", "4", "5"};
         assertEquals("Invalid Answer Choice Input", createQuestionAction("What is 2+2?", a, 3));
     }
 
     @Test
-    void test_five(){
+    void test_five(){ //Invalid answer
         String[] a = {"1", "", "4", "5"};
         assertEquals("Invalid Answer Choice Input", createQuestionAction("What is 2+2?", a, 3));
     }
 
     @Test
-    void test_six(){
+    void test_six(){ //Exceptional answer
         String[] a = {"1", "3", "4", "5", "22", "23", "0", "92", "67", "53", "678", "2453", "24", "781"};
         assertEquals("Invalid Answer Choice Amount", createQuestionAction("What is 2+2?", a, 3));
     }
 
     @Test
-    void test_seven(){
+    void test_seven(){ //Exceptional answer
         String[] a = {};
         assertEquals("Invalid Answer Choice Amount", createQuestionAction("What is 2+2?", a, 3));
     }
 
     @Test
-    void test_eight(){
+    void test_eight(){ //Invalid correct answer
         String[] a = {"1", "3", "4", "5"};
         assertEquals("Invalid Correct Answer Selection", createQuestionAction("What is 2+2?", a, 5));
     }
